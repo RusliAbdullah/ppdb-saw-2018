ab002afc6eaf72a80faddc69a4505958a2a5bfa2
Syahroni Wahyu <roniwahyu@gmail.com>
2018-11-09 14:33:15 +0700

    bootstrap 4 material

--- footer.php
+++ footer.php
@@ -5,18 +5,27 @@
 				<p class="text-muted " style="margin-bottom: 0px;bottom:0px;display:box;position: static">Copyright &copy; 2018</p>
 			</div>
 		</footer>
+			<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script> -->
+			<script src="assets/js/jquery-3.3.1.slim.min.js"></script>
+   			<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script> -->
+   			<script src="assets/js/popper.min.js"></script>
+    		<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script> -->
+    		<script src="assets/js/bootstrap.min.js"></script>
 
+    <!-- Then Material JavaScript on top of Bootstrap JavaScript -->
+   			 <script src="assets/js/material.min.js"></script>
 
 			<!-- Bootstrap CSS -->
-			<link href="assets/css/bootstrap.min.css" rel="stylesheet" media="all">
+			<!-- <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="all"> -->
 			<link href="assets/css/print.css" rel="stylesheet" media="print">
 			<!-- jQuery -->
-			<script src="assets/js/jquery.min.js"></script>
+			<!-- <script src="assets/js/jquery.min.js"></script> -->
 			<!-- Bootstrap JavaScript -->
-			<script src="assets/js/bootstrap.min.js"></script>
+			<!-- <script src="assets/js/bootstrap.min.js"></script> -->
 			
 			<script type="text/javascript">
 				    $(document).ready(function() {	
+				    	$('#tgl_lahir').pickdate();
 				    $('button.print').click(function() {
 					    window.print();
 					    	return false;
