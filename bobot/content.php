82aba475f116c780d5618d72874622e7964abbe0
Syahroni Wahyu <roniwahyu@gmail.com>
2018-11-10 20:50:04 +0700

    update

---
+++ bobot/content.php
@@ -0,0 +1,13 @@
+ <?php if (!defined('baseurl')) exit('No direct script access allowed');?>
+
+ 				<div class="container jumbotron" style="height: 100%;min-height: 624px;">
+ 					<p class="float-right">
+ 						<a href="<?= baseurl.'saw/proses_saw.php'?>" class="btn btn-success btn-lg" href="#" role="button">Proses SAW</a>
+ 					</p>
+ 					<h1 class="display-3"><?= !empty($content_title)?$content_title:'Judul';  ?></h1>
+ 					<?php include('bobot/bobot.php') ?>
+ 					<p class="lead">
+ 						<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+ 					</p>
+ 				</div>
+
